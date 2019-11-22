6cf7dd38772126a106019dd204a2c3214e3432aa
Narushevich Marianna <marianna.naru@gmail.com>
2019-11-22 21:04:01 +0300

    jd01_04 add task text

--- src/by/it/narushevich/jd01_04/InOut.java
+++ src/by/it/narushevich/jd01_04/InOut.java
@@ -6,41 +6,26 @@ class InOut {
      * @param line строка символов, вводится с клавиатуры
      * @return результатом метода становится массив из чисел в формате double
      */
-    static double[] getArray(String line){
+    static double[] getArray(String line) {
         String[] strArr = line.split(" ");
-        double [] result = new double[strArr.length];
+        double[] result = new double[strArr.length];
         for (int i = 0; i < strArr.length; i++) {
-            result[i]=Double.parseDouble(strArr[i]);
+            result[i] = Double.parseDouble(strArr[i]);
         }
         return result;
     }
 
-    static int[] getArrayInteger(String line){
-        String[] strArr = line.split(" ");
-        int [] result = new int[strArr.length];
-        for (int i = 0; i < strArr.length; i++) {
-            result[i]=Integer.parseInt(strArr[i]);
-        }
-        return result;
-    }
-
-    /** Метод обрабатывается следующие параметры:
-     * @param arr - массив из чисел в формате double
-     * @param name - символ, имя элемента
+    /**
+     * Метод обрабатывается следующие параметры:
+     *
+     * @param arr         - массив из чисел в формате double
+     * @param name        - символ, имя элемента
      * @param columnCount - число колонок в отформатированном массиве
      */
-    static void printArray(double[] arr, String name, int columnCount){
+    static void printArray(double[] arr, String name, int columnCount) {
         for (int i = 0; i < arr.length; i++) {
             System.out.printf("%s[% -3d]=%-6.2f ", name, i, arr[i]);
-            if ((i+1)%columnCount==0 || i+1==arr.length)
-                System.out.println();
-        }
-    }
-
-    static void printArrayInteger(int[] arr, String name, int columnCount){
-        for (int i = 0; i < arr.length; i++) {
-            System.out.printf("%s[% -3d]=%-6.2f ", name, i, arr[i]);
-            if ((i+1)%columnCount==0 || i+1==arr.length)
+            if ((i + 1) % columnCount == 0 || i + 1 == arr.length)
                 System.out.println();
         }
     }
